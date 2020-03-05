7eda689af8679092db60c6417a6cf3e9ba272eb4
Jackson Taylor <jtttaylorjackson@gmail.com>
2020-03-04 16:30:17 -0800

    think that i have the base format for how i want to do the otool program

--- otool/inc/ft_otool.h
+++ otool/inc/ft_otool.h
@@ -6,12 +6,20 @@
 /*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:49:56 by jtaylor           #+#    #+#             */
-/*   Updated: 2020/03/03 18:46:31 by jtaylor          ###   ########.fr       */
+/*   Updated: 2020/03/04 16:27:03 by jtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_OTOOL_H
 # define FT_OTOOL_H
+
+# include <fcntl.h>
+# include <sys/mman.h>
+# include <sys/stat.h>
+# include <mach-o/fat.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <ar.h>
 
 # include "libft.h"
 # include "ft_printf.h"
@@ -26,6 +34,12 @@
 /*
 ** structs ---------------------------------------------------------------------
 */
+
+typedef struct		s_ft_otool
+{
+	void		*data;
+	size_t			len;
+}					t_ft_otool;
 
 /*
 ** prototypes ------------------------------------------------------------------
--- otool/src/ft_otool_main.c
+++ otool/src/ft_otool_main.c
@@ -6,11 +6,11 @@
 /*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:48:22 by jtaylor           #+#    #+#             */
-/*   Updated: 2020/03/03 19:45:56 by jtaylor          ###   ########.fr       */
+/*   Updated: 2020/03/04 16:29:00 by jtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fot_otool.h"
+#include "ft_otool.h"
 
 /*
 ** -----------------------------------------------------------------------------
@@ -24,6 +24,35 @@
 */
 
 /*
+** this is so that we don't read outsid the bounds of the memory
+** cur_add - start_addr + len_rto_read < len of memory
+*/
+
+void			*otool_file_read_protect(t_ft_otool *o,
+		void *addr_start, size_t len_to_read)
+{
+	return (((addr_start - o->data) + len_to_read) > o->len) ? 0 : addr_start;
+}
+
+int				otool_handle(void		*data, size_t len)
+{
+	t_ft_otool		o;
+	uint32_t		*has_magic;
+	uint32_t		which_magic;
+
+	o.data = data;
+	o.len = len;
+	has_magic = otool_file_read_protect(&o, o.data, sizeof(uint32_t));
+	if (!has_magic)
+		return (0);
+	which_magic = *(uint32_t *)o.data;
+	//if else chain to pass to function based on which magic it has
+	//
+	//
+	return (0);
+}
+
+/*
 ** open the file -- check that we had permissions to open it and that != dir
 ** alloc memory for the file then pass to the handle func to print
 ** then free // munmap the memory && close the file
@@ -33,6 +62,7 @@ static inline void	otool_open_file(char *file_name)
 {
 	int			fd;
 	struct stat	tmp;
+	void		*data;
 
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
@@ -47,13 +77,18 @@ static inline void	otool_open_file(char *file_name)
 		//even though i didn't do malloc
 		//so something like
 		//alloc memory
+		data = mmap(0, tmp.st_size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+		if (!data)
+			;//exit error alloc mem
 		//pass to handle -- handle alloc fail
+		otool_handle(data, tmp.st_size);
 		//unalloc memory
 		//close fd
 	}
 	else//this is redundent
 		return ;
 	//still not sure if want to play with the munmap
+	munmap(data, tmp.st_size);
 	close(fd);
 }
 
@@ -78,6 +113,7 @@ int		main(int ac, char **argv)
 	if (ac < 2)
 	{
 		ft_dprintf(2, "ft_otool: missing file arglist\n");
-		return (INV_ARG);
+		return (INV_ARGS);
 	}
+	return (0);
 }

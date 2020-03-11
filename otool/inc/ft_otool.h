4d356b1e1179f5341afb585d47e287c458951af5
Jackson Taylor <jtttaylorjackson@gmail.com>
2020-03-10 21:54:27 -0700

    need to change how otool prio's 32 vs 64 bit arch printing format , also when outputting an exec we want to print the offset from the filestart and not from the mach-o header start ? im not really sure how it differentiates them

--- otool/inc/ft_otool.h
+++ otool/inc/ft_otool.h
@@ -6,27 +6,37 @@
 /*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:49:56 by jtaylor           #+#    #+#             */
-/*   Updated: 2020/03/10 20:45:17 by jtaylor          ###   ########.fr       */
+/*   Updated: 2020/03/10 21:17:46 by jtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_OTOOL_H
 # define FT_OTOOL_H
 
-# include <fcntl.h> //open
-# include <sys/mman.h> //mmap
-# include <sys/stat.h>//fstat
-# include <mach-o/fat.h> //for fat binarys
-# include <mach-o/loader.h> // mach-o
-# include <mach-o/nlist.h> //for bsd / see the actual .h file
-# include <ar.h> // if we wanna handle arhive'x
+/*
+** # include <fcntl.h> //open
+** # include <sys/mman.h> //mmap
+** # include <sys/stat.h>//fstat
+** # include <mach-o/fat.h> //for fat binarys
+** # include <mach-o/loader.h> // mach-o
+** # include <mach-o/nlist.h> //for bsd / see the actual .h file
+** # include <ar.h> // if we wanna handle arhive'x
+*/
+
+# include <fcntl.h>
+# include <sys/mman.h>
+# include <sys/stat.h>
+# include <mach-o/fat.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <ar.h>
 
 # include "libft.h"
 # include "ft_printf.h"
 # include "shared_func.h"
 
 # define INV_ARGS 0x01
-# define FT_OTOOL_USG "A usefull help message" // fix me later
+# define FT_OTOOL_USG "A usefull help message"
 # define FT_OTOOL_TEXT "Contents of (__TEXT,__text) section"
 
 /*
@@ -71,8 +81,10 @@ typedef struct		s_ft_otool
 ** prototypes ------------------------------------------------------------------
 */
 
-void				handle_mach_o_64(t_ft_otool *o, char *file_name, int swap_end);
-void				handle_mach_o_32(t_ft_otool *o, char *file_name, int swap_end);
+void				handle_mach_o_64(t_ft_otool *o, char *file_name,
+						int swap_end);
+void				handle_mach_o_32(t_ft_otool *o, char *file_name,
+						int swap_end);
 int					otool_handle(void		*data, size_t len, char *file_name);
 int					handle_fat_binary(t_ft_otool *o, char *file_name);
 int					handle_archive(t_ft_otool *o, char *file_name);

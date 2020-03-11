1557d0c6008f8e0529a6cb2d99c9dfb243a9e1dc
Jackson Taylor <jtttaylorjackson@gmail.com>
2020-03-10 18:25:25 -0700

    need to finish the updated version of the archive , this should be a lot cleaner just need to actually move to the correct address in the file

--- otool/src/handle_archive.c
+++ otool/src/handle_archive.c
@@ -6,7 +6,7 @@
 /*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 11:18:36 by jtaylor           #+#    #+#             */
-/*   Updated: 2020/03/09 20:43:41 by jtaylor          ###   ########.fr       */
+/*   Updated: 2020/03/10 18:24:31 by jtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,16 @@
 ** mach-o parser and print the file_name
 */
 
+/*
+** this version is very broken ,
+** im moving the pointers by the wrong offset so im just gonna rewrite this
+** func because that's gonna be faster than finding whats broken
 int				handle_archive(t_ft_otool *o, char *file_name)
 {
 //	(void)o;
 //	(void)file_name;
 	struct ar_hdr		*ar_header;
-	struct ar_hdr		*tmp;
+	void				*tmp;
 	size_t				i;
 
 	ft_printf("Archive : %s\n", file_name);
@@ -47,17 +51,43 @@ int				handle_archive(t_ft_otool *o, char *file_name)
 									//SARMAGIC == sizeof AR_MAGIC
 	tmp = ar_header;//move to the end of the header
 	//ft_printf("ar_header->ar_name ::%s\n", ar_header->ar_name);
-	while (tmp && (tmp += ft_atoi(tmp->ar_size) + sizeof(struct ar_hdr)))
+	while ((struct ar_hdr *)tmp && (tmp += ft_atoi(((struct ar_hdr *)tmp)->ar_size) + sizeof(struct ar_hdr)))//go throught the archive headers
 	{
-		ar_header = tmp;
+		ft_printf("inside\n");
+		ar_header = (struct ar_hdr *)tmp;
 		if ((i = ft_atoi(ar_header->ar_size) && !i))
 			return (-1);//exit invalid archive size
 		if ((void *)tmp + i + sizeof(struct ar_hdr) > o->data + o->len)
 			return (-1);//would read out of the file array
-		ft_printf("%s(%s)\n", file_name, (char *)tmp->ar_name - 8);// this is the wrong offset
+		ft_printf("%s(%s)\n", file_name, (char *)((struct ar_hdr *)tmp)->ar_name - 8);// this is the wrong offset
 		otool_handle(ft_atoi(((struct ar_hdr *)tmp)->ar_size) + (void *)tmp + sizeof(struct ar_hdr),
 			o->len - (size_t)(ft_atoi(((struct ar_hdr *)tmp)->ar_size) + (void *)tmp + sizeof(struct ar_hdr)), file_name);
 		//tmp = 0;
+	}
+	return (0);
+}
+*/
+
+int					handle_archive(t_ft_otool *o, char *file_name)
+{
+	void				*tmp;
+	void				*tmp2;
+	struct ar_hdr		*ar_header;
+	uint64_t			i;
+
+	i = 0;
+	tmp = o->data + SARMAG;
+	ar_header = (struct ar_hdr *)tmp;
+	//write(1, &header->ar_name[0], 16);
+	while (tmp && (tmp += ft_atoi(ar_header->ar_size) + sizeof(struct ar_hdr)))
+	{
+		ar_header = tmp;
+		if (((i = ft_atoi(ar_header->ar_size)) && !i) || (o->len - (tmp - o->data) <= 0))
+			return (-1);//corrupt archive header
+		tmp2 = (void *)ar_header + sizeof(struct ar_hdr);//correct offset for the obj_name but not the actual obj data
+		ft_printf("%s(%s)\n", file_name, (char *)tmp2);
+		otool_handle(tmp2, (o->len - (tmp2 - o->data)), file_name);
+		break;//
 	}
 	return (0);
 }
